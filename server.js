081aafa604117090e46d15d5b8497e61a269f2f9
Edis Krsmanović <ediskrsmanovic@gmail.com>
2021-05-18 21:23:16 +0200

    Postavka

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(3000);
