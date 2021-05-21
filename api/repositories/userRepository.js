b27d405fce39c7b1603779b028eb1fbff6c4cf4b
Samija Kujrakovic <skujrakovi1@etf.unsa.ba>
2021-05-21 11:32:14 +0200

    implemented user crud routes

--- api/repositories/userRepository.js
+++ api/repositories/userRepository.js
@@ -1,7 +1,35 @@
 const sqlite = require('sqlite-sync');
+const bcrypt = require("bcrypt");
 sqlite.connect('baza.db')
 
 exports.getUser = function (email) {
     const user = sqlite.run('SELECT * FROM User WHERE Email=?', [email]);
     return user[0];
+}
+
+exports.addUser = function (user) {
+    hashedPass = bcrypt.hashSync(user.Password, 12, function (err1, hashedPassword) {
+        if(err1){
+            console.log("error1");
+            return "NOT OK";
+        }
+    });
+    sqlite.run('INSERT INTO User(ID,Email,Password,Uloga) VALUES (?,?,?,?)',[null,user.Email, hashedPass, user.Uloga], function(err) {
+        console.log('User successfully added.');
+      });;
+    return "OK";
+}
+
+exports.removeUser = function (email) {
+    sqlite.run('DELETE FROM User WHERE Email=?',[email]);
+    return "OK";
+}
+
+exports.updateUser = function (info) {
+    sqlite.run("UPDATE User SET Uloga=? WHERE Email=?", [info.Uloga, info.Email]);
+    return "OK";
+}
+
+exports.getUsers = function () {
+    return sqlite.run('SELECT Email, Uloga FROM User');
 }
