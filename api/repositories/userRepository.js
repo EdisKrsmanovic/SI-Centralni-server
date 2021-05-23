b3746a01cdbbf894764620afaf68a98b62608aea
Samija Kujrakovic <skujrakovi1@etf.unsa.ba>
2021-05-23 17:19:20 +0200

    blabla

--- api/repositories/userRepository.js
+++ api/repositories/userRepository.js
@@ -1,7 +1,16 @@
 const sqlite = require('sqlite-sync');
 const bcrypt = require("bcrypt");
-sqlite.connect('baza.db')
-const bcrypt = require("bcrypt");
+sqlite.connect('baza.db');
+const pg = require('pg');
+const pool = new pg.Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }
+}
+);
 
 exports.getUser = function (email) {
     const user = sqlite.run('SELECT * FROM User WHERE Email=?', [email]);
@@ -15,9 +24,11 @@ exports.addUser = function (user) {
             return "NOT OK";
         }
     });
-    sqlite.run('INSERT INTO User(ID,Email,Password,Uloga) VALUES (?,?,?,?)',[null,user.Email, hashedPass, user.Uloga], function(err) {
+    pool.query("INSERT INTO \"User\"(\"ID\",\"Email\",\"Password\",\"Uloga\") VALUES ((SELECT MAX(\"UserId\") + 1 FROM \"USER\"),$1::text,$2::text,$3::text)",[user.Email, hashedPass, user.Uloga]).then(dbResponse=> {
         console.log('User successfully added.');
-      });;
+      }, err1 => {
+        console.log(err1.message);
+    });
     return "OK";
 }
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -5,12 +6,13 @@ const app = express();
 const userRoutes = require('./api/routes/userRoutes');
 const authRoutes = require('./api/routes/authRoutes');
 const faDefintionRoutes = require('./api/routes/faDefintionRoutes');
+port = process.env.PORT || 3333
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server uspješno pokrenut!')
 });
 
